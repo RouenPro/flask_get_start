af615ee0e9c0dd1ce9a0cd5522dc969671e241c0
chamrouen <sannchamrouen17@kit.edu.kh>
2020-02-27 23:26:38 +0700

    Fixxing the Int

--- VariableRule/app.py
+++ VariableRule/app.py
@@ -2,17 +2,18 @@ from flask import Flask
 from markupsafe import escape
 
 app = Flask(__name__)
+
 @app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return 'User %s' % escape(username)
+def user_name(username):
+    return 'This is %s' %escape(username)
 
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return 'Post %d' % post_id
+@app.route('/userid/<int:userid>')
+def user_id(userid):
+    return 'This is userid %s' %userid
 
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    # show the subpath after /path/
-    return 'Subpath %s' % escape(subpath)
+
+@app.route('/userpath/<path:userpath>')
+def user_path(userpath):
+    return 'This is userpath %s' %escape(userpath)
+
+
