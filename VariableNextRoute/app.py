3811b2e0bb0d2df8d620b2245c9518363ca75779
chamrouen <sannchamrouen17@kit.edu.kh>
2020-02-28 09:27:25 +0700

    Commiting the UUID code UUID: fe4a3e33-cb4b-42ac-a29b-a8160a85af7e

--- VariableNextRoute/app.py
+++ VariableNextRoute/app.py
@@ -4,8 +4,13 @@ from markupsafe import escape
 app = Flask(__name__)
 @app.route('/')
 def test():
-    return 'R'
+    return 'First welcome route'
 
 @app.route('/user/<float:number>')
 def test_float(number):
     return 'This is float number %s' %escape(number)
+
+# UUID: fe4a3e33-cb4b-42ac-a29b-a8160a85af7e
+@app.route('/usercode/<uuid:code_generator>')
+def the_password(code_generator):
+    return 'This is your password %s' %escape(code_generator)
