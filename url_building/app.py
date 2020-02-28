0aa15ee19c07abbdb09c998dcf134e6c0451ce15
chamrouen <sannchamrouen17@kit.edu.kh>
2020-02-28 10:40:06 +0700

    Comming the URL Building for url for

--- url_building/app.py
+++ url_building/app.py
@@ -6,6 +6,25 @@ app = Flask(__name__)
 def main():
     return 'test'
 
+@app.route('/login')
+def logincheck_url():
+    return 'Login successfully'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % escape(username)
+
+@app.route('/userurl/<usernames>')
+def usernames_check(usernames):
+    return '{}\'s my fri' .format(escape(usernames))
+
 # Testing the main function
 with app.test_request_context():
     print(url_for('main'))
+    print(url_for('logincheck_url'))
+    print(url_for('logincheck_url', next='/'))
+    print(url_for('show_user_profile',username='Chamrouen'))
+    print(url_for('usernames_check', usernames='Rouen'))
+    # print(url_for('show_user_profile',next='/'))
+    # print(url_for('usernames_check', usernames='RouenPro'))
